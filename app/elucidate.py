3b5b51a4e8240a58f32dd841476948dec4c037d2
Matthew McGrattan <matthew.mcgrattan@bodleian.ox.ac.uk>
2018-03-22 22:54:43 +0000

    Async version of the manifests from topic added using asyncio and aiohttp.

--- app/elucidate.py
+++ app/elucidate.py
@@ -1,5 +1,26 @@
 import requests
 from urllib.parse import parse_qsl, urlencode, urlparse, urlunparse, quote_plus, parse_qs
+import asyncio
+from aiohttp import ClientSession, TCPConnector
+
+
+async def fetch_all(urls):
+    """Launch requests for all web pages."""
+    tasks = []
+    fetch.start_time = dict() # dictionary of start times for each url
+    async with ClientSession(connector=TCPConnector(limit=5)) as session:
+        for url in urls:
+            task = asyncio.ensure_future(fetch(url, session))
+            tasks.append(task) # create list of tasks
+        results = await asyncio.gather(*tasks) # gather task responses
+        return results
+
+
+async def fetch(url, session):
+    """Fetch a url, using specified ClientSession."""
+    async with session.get(url) as response:
+        resp = await response.json()
+        return resp
 
 
 def set_query_field(url, field, value, replace=False):
@@ -34,6 +55,20 @@ def annotation_pages(result):
             yield page
     else:
         return
+
+
+def items_by_topic_async(elucidate, topic):
+    t = quote_plus(topic)
+    sample_uri = elucidate + 'annotation/w3c/services/search/body?fields=source&value=' + t
+    print(sample_uri)
+    r = requests.get(sample_uri)
+    if r.status_code == requests.codes.ok:
+        loop = asyncio.get_event_loop()  # event loop
+        future = asyncio.ensure_future(fetch_all([p for p in annotation_pages(r.json())]))  # tasks to do
+        pages = loop.run_until_complete(future)  # loop until done
+        for page in pages:
+            for item in page['items']:
+                yield item
 
 
 def items_by_topic(elucidate, topic):
@@ -72,3 +107,13 @@ def manifests_by_topic(elucidate='https://elucidate.dlcs-ida.org/', topic=None):
             m = manifest_from_annotation(anno)
             if m:
                 yield m
+
+
+def manifests_by_topic_async(elucidate='https://elucidate.dlcs-ida.org/', topic=None):
+    if topic:
+        return [manifest_from_annotation(anno) for anno in items_by_topic_async(elucidate, topic)]
+
+
+for x in manifests_by_topic_async(elucidate='https://elucidate.dlcs-ida.org/',
+                            topic='https://omeka.dlcs-ida.org/s/ida/page/topics/virtual:norp/indian'):
+    print(x)
